916b863df0c06ee61a7f92d5ec4f0386cbf33926
JK4302 <72574346+JK432@users.noreply.github.com>
2022-01-22 05:44:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write a python program to compare two dictionaries
+
+dict1 = {'Name': 'Tom Cruise', 'Age':59}
+dict2 = {'Name': 'Leonardo DiCaprio', 'Age': 47}
+  
+if dict1 == dict2:
+    print ("equal ")
+else:
+    print (" not equal")
